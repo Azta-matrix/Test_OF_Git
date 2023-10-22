435b80a4d13e4385377ac58f26cdd106b805e8b8
Azta Matrix <alvarojg2008@gmail.com>
2023-10-22 02:42:14 -0500

    I'm still here, and I will continue chambeando

--- test_OFGit.lua
+++ test_OFGit.lua
@@ -1,1 +1,23 @@
 print("This is a git test, I'm really very new to git, and I have no idea how this is going to turn out. (print too long)")
+
+-- Función para calcular números de Fibonacci
+function CalcularFibonacci(n)
+    local a, b = 0, 1
+    local fib = {}
+
+    for i = 1, n do
+        table.insert(fib, a)
+        a, b = b, a + b
+    end
+
+    return fib
+end
+
+-- Límite superior para los números de Fibonacci
+local limite = 20
+
+-- Calcular y mostrar los números de Fibonacci hasta el límite
+local fibonacci = CalcularFibonacci(limite)
+for i, valor in ipairs(fibonacci) do
+    print(string.format("Fibonacci(%d) = %d", i, valor))
+end
